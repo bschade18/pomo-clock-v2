426043f8041f98a4b81f067d6b03f711a869910a
Bobby Schade <bschade.18@gmail.com>
2020-01-31 07:31:03 -0600

    Updates

---
+++ precache-manifest.55c65c3fa486bda5b0cae27ac33df080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47b9b5c6716c79e6031880dc64117a3",
+    "url": "/pomo-clock-v2/index.html"
+  },
+  {
+    "revision": "5eb73bf9cd64f0513ccf",
+    "url": "/pomo-clock-v2/static/css/main.83a95813.chunk.css"
+  },
+  {
+    "revision": "133ddeed78c00be3ca7d",
+    "url": "/pomo-clock-v2/static/js/2.08ac96b6.chunk.js"
+  },
+  {
+    "revision": "5eb73bf9cd64f0513ccf",
+    "url": "/pomo-clock-v2/static/js/main.3f747f8f.chunk.js"
+  },
+  {
+    "revision": "59ad36c2f4cf25a01834",
+    "url": "/pomo-clock-v2/static/js/runtime~main.ae3a045c.js"
+  }
+]);
