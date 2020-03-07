b01323098ff37f4687bd1109ca0d90a5b93164f2
Bobby Schade <bschade.18@gmail.com>
2020-03-07 11:31:19 -0600

    refactor: consolidated css with more classes

--- src/components/ControlClock.js
+++ src/components/ControlClock.js
@@ -4,12 +4,16 @@ function ControlClock(props) {
   return (
     <div className="row">
       <div className="col-12">
-        <div className="controls">
-          <div id="play-pause-btn" onClick={props.startPause}>
+        <div className="controls-container">
+          <div
+            id="play-pause-btn"
+            className="control-btn"
+            onClick={props.startPause}
+          >
             <i className="fa fa-play"></i>
             <i className="fa fa-pause"></i>
           </div>
-          <div id="reset-btn" onClick={props.reset}>
+          <div id="reset-btn" className="control-btn" onClick={props.reset}>
             <i className="fas fa-undo"></i>
           </div>
         </div>
--- src/components/SessionLength.js
+++ src/components/SessionLength.js
@@ -4,25 +4,49 @@ function SessionLength(props) {
   return (
     <div className="row">
       <div className="col-6">
-        <p id="break-label">Break Length</p>
-        <p id="break-length">{props.breakLength}</p>
-        <div className="interval-btns">
-          <p id="break-decrement" onClick={props.breakDecrement}>
+        <p id="break-label" className="interval-label">
+          Break Length
+        </p>
+        <p id="break-length" className="interval-length">
+          {props.breakLength}
+        </p>
+        <div className="interval-btn-container">
+          <p
+            id="break-decrement"
+            className="interval-btn"
+            onClick={props.breakDecrement}
+          >
             <i className="fas fa-minus"></i>
           </p>
-          <p id="break-increment" onClick={props.breakIncrement}>
+          <p
+            id="break-increment"
+            className="interval-btn"
+            onClick={props.breakIncrement}
+          >
             <i className="fas fa-plus"></i>
           </p>
         </div>
       </div>
       <div className="col-6">
-        <p id="session-label">Session Length</p>
-        <p id="session-length">{props.sessionLength}</p>
-        <div className="interval-btns">
-          <p id="session-decrement" onClick={props.sessionDecrement}>
+        <p id="session-label" className="interval-label">
+          Session Length
+        </p>
+        <p id="session-length" className="interval-length">
+          {props.sessionLength}{" "}
+        </p>
+        <div className="interval-btn-container">
+          <p
+            id="session-decrement"
+            className="interval-btn"
+            onClick={props.sessionDecrement}
+          >
             <i className="fas fa-minus"></i>
           </p>
-          <p id="session-increment" onClick={props.sessionIncrement}>
+          <p
+            id="session-increment"
+            className="interval-btn"
+            onClick={props.sessionIncrement}
+          >
             <i className="fas fa-plus"></i>
           </p>
         </div>
